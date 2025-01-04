4b81a45e5ad34389a295668fcc6cddad8284e534
Hugo Montes <132196565+hugomonntes@users.noreply.github.com>
2025-01-04 16:32:55 +0100

    Update

--- BD_Tema5.sql
+++ BD_Tema5.sql
@@ -1,11 +1,65 @@
-EJERCICIO GRUPAL TEMA5
+1.	SELECT APELLIDO, OFICIO, CODEMP FROM `empleados`;
+2.	SELECT IDDEPART, NOMBRE, LOC FROM `depart`;
+3.	SELECT * FROM `empleados` ORDER BY APELLIDO;
+4.	SELECT * FROM `empleados` ORDER BY IDDEPART DESC;
+5.	SELECT * FROM `empleados` ORDER BY IDDEPART DESC, APELLIDO;
+6.	SELECT * FROM `empleados` WHERE SALARIO > 2000;
+7.	SELECT * FROM `empleados` WHERE OFICIO = 'Analista';
+8.	SELECT APELLIDO, OFICIO FROM `empleados` WHERE IDDEPART=20;
+9.	SELECT * FROM `empleados` WHERE SALARIO >= 3000 AND IDDEPART !=20 ORDER BY SALARIO, APELLIDO;
+10.	SELECT * FROM `empleados` WHERE IDDEPART = 30 AND oficio = 'vendedor' ORDER BY apellido;
+11.	SELECT * FROM `empleados` WHERE salario BETWEEN 1000 AND 2000;
+12.	SELECT * FROM `empleados` WHERE oficio = 'vendedor' AND comision > 1000;
+13.	SELECT CODEMP, APELLIDO FROM `empleados` WHERE apellido LIKE '%Z' AND salario > 1000;
+14.	SELECT * FROM `empleados` WHERE oficio = 'empleado' AND salario > 1000 AND IDDEPART = 10;
+15.	SELECT APELLIDO FROM `empleados` WHERE comision IS NULL;
+16.	SELECT APELLIDO FROM `empleados` WHERE (comision IS NULL OR comision = 0) AND (apellido LIKE 'J%' OR apellido LIKE 'T%');
+17.	SELECT APELLIDO, OFICIO FROM `empleados` WHERE oficio IN ('vendedor', 'analista', 'empleado');
+18.	SELECT APELLIDO, OFICIO FROM `empleados` WHERE oficio NOT IN ('analista', 'empleado') AND salario > 2000;
+19.	SELECT APELLIDO, SALARIO, IDDEPART FROM `empleados` WHERE salario > 2000 AND (IDDEPART = 10 OR IDDEPART = 30);
+20.	SELECT APELLIDO, CODEMP FROM `empleados` WHERE SALARIO NOT BETWEEN 1000 AND 2000;
+21.	SELECT APELLIDO, IDDEPART  FROM `empleados` WHERE FECHA_ALT < "2011-08-01";
+22.	SELECT apellido, BIN(LENGTH(apellido)) AS Binario FROM empleados;
+23.	SELECT apellido, LENGTH(apellido) AS longitud FROM empleados ORDER BY LENGTH(apellido) DESC LIMIT 5;
+24.	SELECT CONCAT(apellido, ' ', oficio) AS listado FROM empleados;
+25.	SELECT * FROM empleados WHERE OFICIO = 'Analista' AND YEAR(FECHA_ALT) = 2011;
+26.	SELECT * FROM empleados WHERE YEAR(FECHA_ALT) = 2012;
+27.	SELECT * FROM empleados WHERE MONTH(FECHA_ALT) = 3;
+28.	SELECT apellido, FECHA_ALT, DATE_FORMAT(FECHA_ALT + INTERVAL 10 YEAR, '%Y-%m-%d') AS "diez_años_mas" FROM empleados;
+29. 	SELECT  apellido, (salario + comision) AS cobro FROM empleados WHERE OFICIO IN ('vendedor', 'analista');
+   	SELECT apellido, (COALESCE(salario, 0) + COALESCE(comision, 0)) AS cobro FROM empleados WHERE OFICIO IN ('vendedor', 'analista');
+30.	SELECT APELLIDO, IFNULL(COMISION, 0) AS 'Comisiones', COALESCE(comision, 0) AS ‘comisiones2’ FROM empleados;
+31.	SELECT * FROM empleados WHERE apellido LIKE 'A%' AND YEAR(FECHA_ALT) = 2012;
+32.	SELECT apellido, salario * SQRT(5) AS nuevo_salario FROM empleados;
+33.	SELECT apellido, FECHA_ALT, DATE_FORMAT(FECHA_ALT, '%W') AS "dia", DAYOFWEEK(FECHA_ALT) AS "nº dia" FROM empleados;
+34.	SELECT COUNT(*) AS "nun emple", COUNT(DISTINCT oficio) AS "nun oficios" FROM empleados;
+35.	SELECT APELLIDO, GREATEST(codemp,SALARIO, IFNULL(COMISION, 0),IDDEPART) AS "mayor" from empleados;
+36.	SELECT  apellido, AES_ENCRYPT(apellido, oficio) AS clave FROM empleados;
+37.	SELECT apellido, AES_ENCRYPT(apellido, oficio) AS clave, AES_DECRYPT(AES_ENCRYPT(apellido, oficio), oficio) AS usuario FROM empleados;
+38.	SELECT TIME(NOW()) AS "hora", CURRENT_USER() AS "usuario", DATABASE() AS "base de datos";
+39.	SELECT REPLACE('hola mundo', 'ola', 'ello') AS “remplazado”;
+40.	SELECT MAX(FECHA_ALT) AS "fecha mas alta" FROM empleados;
+41.	SELECT apellido, LEFT(apellido, 4) AS "4 carac" FROM empleados;
+42.	SELECT FECHA_ALT, DATE_ADD(DATE_ADD(FECHA_ALT, INTERVAL 1 YEAR), INTERVAL 2 MONTH) AS "fecha" FROM empleados;
+43.	SELECT LOWER(APELLIDO) AS "minusculas" FROM empleados;
+44.	SELECT APELLIDO, SALARIO, FLOOR(SALARIO / 1000) * 1000 AS "miles" FROM empleados;
+45.	SELECT * FROM `empleados` WHERE OFICIO LIKE 'Vendedor';
+	SELECT * FROM `empleados` WHERE OFICIO = 'Vendedor';
+	SELECT * FROM `empleados` WHERE LOCATE(OFICIO,'Vendedor') ;
+46.	SELECT * FROM `empleados` WHERE APELLIDO LIKE 'A%';
+	SELECT * FROM empleados WHERE SUBSTRING(apellido, 1, 1) = 'A';
+47.	SELECT * FROM empleados WHERE apellido LIKE 'F%' OR (oficio LIKE '%E_' AND oficio NOT LIKE '%E');
+48.	SELECT * FROM empleados WHERE apellido LIKE 'F%' OR SUBSTRING(oficio, LENGTH(oficio) - 1, 1) = 'E';
+49.	SELECT APELLIDO, IDDEPART, HEX(IDDEPART) AS "hexa" FROM empleados;
+50.	SELECT GROUP_CONCAT(apellido SEPARATOR ', ') AS apellido, POW(COUNT(*), 3) AS "cubo" FROM empleados;
+51.	SELECT apellido, RIGHT(apellido, 3) AS “3carac”, CASE WHEN apellido = REVERSE(apellido) THEN 'Sí' ELSE 'No' END AS palindromo, 
+	CASE WHEN RIGHT(apellido, 3) = REVERSE(RIGHT(apellido, 3)) THEN 'Sí' ELSE 'No' END AS "palindromo2" FROM empleados;
+52.	SELECT APELLIDO, DATEDIFF(DATE(NOW()), FECHA_ALT) AS "dias", SLEEP(5) AS "dormir" FROM empleados WHERE APELLIDO = 'Arroyo';
+53.	SELECT DATEDIFF( (SELECT MAX(FECHA_ALT) FROM empleados), (SELECT MIN(FECHA_ALT) FROM empleados) ) AS "dias";
+54.	SELECT apellido FROM empleados WHERE SUBSTRING(apellido, LENGTH(apellido) / 2) LIKE '%E%';
+55.	SELECT apellido, STRCMP(binary apellido, UPPER(apellido)) AS "con minuscula" FROM empleados;
+56.	SELECT APELLIDO, SALARIO, ROUND(SALARIO,-3) AS “nuevo salario” FROM `empleados`;
 
-1. SELECT APELLIDO, OFICIO, CODEMP FROM empleados;
-2. SELECT IDDEPART, NOMBRE, LOC FROM `depart`;
-3. SELECT * FROM `empleados` ORDER BY APELLIDO;
-4. SELECT * FROM `empleados` ORDER BY IDDEPART DESC;
-5.
-6. SELECT * FROM `
 
 CREATE DATABASE IF NOT EXISTS `tema5`;
 USE `tema5`;
