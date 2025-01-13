2d7cfa8fd582cdacc803732da018eccafb5d02fd
hugomonntes <huugomontees@gmail.com>
2025-01-13 13:15:11 +0100

    Update

--- BD_Creates.sql
+++ BD_Creates.sql
@@ -1,3 +1,5 @@
+    **CREAR BORRAR O COPIAR TABLAS/DATABASES**
+
 1. CREATE DATABASE IF NOT EXISTS OURENSE CHARACTER = LATIN1; /*Crear tabla si no existe*/
 2. DROP DATABASE IF EXISTS OURENSE; /*Borrar tabla si existe*/
 3. CREATE OR REPLACE DATABASE OURENSE; /*Crear o reemplazar base de datos o tabla*/
@@ -5,3 +7,16 @@
 5. RENAME TABLE CLASE.ALUMNOS TO COLEGIO.ALUMNOS; /*Mover una tabla a otra base de datos *(NO TRASPASA PRIMARY KEYS NI COLUMNAS AUTO_INCREMENTS, NI FECHAS ACTUALES)*/
 6. CREATE TABLE ESTUDIANTES (SELECT * FROM ALUMNOS);
 7. CREATE TABLE ESTUDIANTES LIKE ALUMNOS; /*Copia la estructura de la tabla no los datos de dentro*/
+
+    **INDICES** --> Mejoran la velocidad y la eficiencia de las consultas
+
+1. CREATE TABLE ALUMNOS ( --> Forma Optimizada  
+    ID INT,
+    NOMBRE VARCHAR(30),
+    PRIMARY KEY(ID)
+)
+
+1.1 CREATE TABLE ALUMNOS ( --> Forma NO Optimizada
+    ID INT PRIMARY KEY,
+    NOMBRE VARCHAR(30)
+)
