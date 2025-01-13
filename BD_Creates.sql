30d9423577355ffb31767c0c51864e30d31516cb
hugomonntes <huugomontees@gmail.com>
2025-01-13 13:25:49 +0100

    Update

--- BD_Creates.sql
+++ BD_Creates.sql
@@ -8,15 +8,48 @@
 6. CREATE TABLE ESTUDIANTES (SELECT * FROM ALUMNOS);
 7. CREATE TABLE ESTUDIANTES LIKE ALUMNOS; /*Copia la estructura de la tabla no los datos de dentro*/
 
-    **INDICES** --> Mejoran la velocidad y la eficiencia de las consultas
+    **INDICES PRIMARY KEY** --> Mejoran la velocidad y la eficiencia de las consultas
 
 1. CREATE TABLE ALUMNOS ( --> Forma Optimizada  
     ID INT,
     NOMBRE VARCHAR(30),
-    PRIMARY KEY(ID)
+    PRIMARY KEY(ID, NOMBRE) --> Clave primaria compuesta entre esas dos columnas
 )
 
 1.1 CREATE TABLE ALUMNOS ( --> Forma NO Optimizada
     ID INT PRIMARY KEY,
     NOMBRE VARCHAR(30)
+)
+
+    **INDICES NORMALES** --> INDEX O KEY
+
+2. CREATE TABLE ALUMNOS ( 
+    ID INT,
+    NOMBRE VARCHAR(30),
+    INDEX(NOMBRE) --> Mejora la eficiencia de la consulta
+)
+
+2.1 CREATE TABLE ALUMNOS (
+    ID INT,
+    NOMBRE VARCHAR(30),
+    KEY(NOMBRE(4)) --> Hace lo mismo que index() el parámetro acota los nombres al número de letras introducido Pedro --> Pedr
+)
+
+2.2 CREATE TABLE ALUMNOS ( 
+    ID INT,
+    NOMBRE VARCHAR(30),
+    UNIQUE(NOMBRE) --> Crea un índice y es único
+)
+
+    **FOREAN KEYS**
+
+3. CREATE TABLE PERSONAS ( 
+    ID INT PRIMARY KEY,
+    NOMBRE VARCHAR(40),
+)
+
+3.1 CREATE TABLE TELEFONOS ( 
+    ID INT NOT NULL,
+    NUM CHAR(12),
+    PERSONA INT NOT NULL
 )
