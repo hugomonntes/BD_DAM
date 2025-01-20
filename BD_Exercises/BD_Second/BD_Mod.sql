7dff1605d2c7b1648355e4e8bb21444ea6365002
hugomonntes <huugomontees@gmail.com>
2025-01-20 16:03:28 +0100

    Update

---
+++ BD_Exercises/BD_Second/BD_Mod.sql
@@ -0,0 +1,28 @@
+/*
+20. Inserta una nueva columna llamada nueva de tipo entero y que puede tomar
+valores nulos y que por defecto puede tomar el valor 0 después de la
+columna juego en ventas.
+21. Inserta en única sentencia las columnas de tipo entero nueva2 y nueva3 en
+la tabla ventas.
+22. Cambia el nombre de las columnas creadas en al apartado anterior de nueva
+o novas y el tipo a varchar.
+23. Modifica el tipo de nova2 a entero.
+24. Elimina la columna nova3.
+25. Borra de la tabla socios el campo único.
+26. Borra de la tabla socios el índice sobre Apellidos.
+27. Modifica el campo numeroSocio en socios y defínelo como entero. ¿Qué
+sucede?¿Cómo lo solucionarías?
+28. Borra la clave primaria de la tabla alquileres. ¿Qué sucede? ¿Cómo lo
+solucionarías?
+29. Inserta de nuevo la clave primaria sobre el mismo campo.
+30. Inserta un índice sobre el campo DNI de la tabla socios.
+31. Crea un índice sobre el nombre de un socio llamado indiceSocio. Comprueba
+que el índice tiene el nombre correcto.
+32. Borra el índice anterior.
+33. Crea un índice único entre el nombre y apellidos de la tabla socios llamado
+nombre completo.
+34. Borra el índice anterior.
+35. Borra las claves foráneas de la tabla ventas.
+36. Vuelve a crear las claves foráneas.
+37. Cambia el motor de almacenamiento de la tabla ventas a MyISAM
+*/
