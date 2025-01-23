bccf115711aa620661bfc72507b3ae1ecb069beb
hugomonntes <huugomontees@gmail.com>
2025-01-23 12:21:13 +0100

    Update

---
+++ BD_Exercises/BD_Second/BD_Insert.sql
@@ -0,0 +1,27 @@
+--8
+INSERT TABLE JUEGOS (NOMBRE, PLATAFORMA, PRECIOVENTA, PRECIOALQUILER, STOCKVENTA, STOCKALQUILER) VALUES (
+    'Maniac Mansion', "Nintendo,DS,PC", 40.30, 5, 10, 5,
+    'Indiana Jones and the Last Crusade', "PlayStation, Xbox, PC", 50, NULL, 53, NULL,
+    'Loom', "Wii, PC", NULL, 5, 13, 6,
+    'Monkey Island II: The LeChucks Revenge', "Wii,PSP,PC", 20, NULL , 4 , 2,
+);
+
+-- 3 Loom Wii PC 5 13 6
+-- 5 Day of the Tentacle XBOX PC 50.50 4 1
+-- 6 Sam & Max Hit the Road PSP Wii PC Playstation 37.30 5.2 2 4
+-- 7 Full Throttle PSP Wii Playstation
+-- XBOX PC Nintendo DS 42.00 3.50 4
+-- 8 The Dig PC 50.2 4 5
+-- 9 La abadía del crimen PC 25 5 4 3
+
+--10
+INSERT TABLE SOCIOS (NOMBRE, APELLIDOS, DNI, PAIS, SEXO, OBSERVACIONES) VALUES(
+    'Paco', 'Menéndez', '11111', 'España', 'H', 'Querido Asdo, ¿No confundirás el amor con la lujuria?', 
+    'Ron', 'Gilbert', '22222', 'EEUU', 'H', 'Miren ¡Un mono de tres cabezas!',
+    'John', 'Carmack', '33333', 'EEUU', 'H', 'El padre del shooter',
+    'Shigeru', 'Miyamoto', '44444', 'Japón', 'H', 'Zeruda no Densetsu'
+    'Alekséi', 'Pázhitnov', '55555', 'Rusia', 'H', 'Demasiadas piezas',
+    'Nolan', 'Bushnell', '6666', 'EEUU', 'H', 'Dos raquetas y una pelota',
+    'John', 'Tobias', '7777', 'EEUU', 'H', 'Test your might',
+    'Peter', 'Molyneux', '8888', 'UK', 'H', 'Juega a ser Dios'
+);
