402c51cc46d89031be1c07f06d5fbb47c834a61e
hugomonntes <huugomontees@gmail.com>
2025-02-10 13:48:22 +0100

    Update

---
+++ BD_Exercises/BD_Second/BD_Exports.sql
@@ -0,0 +1,6 @@
+--44
+SELECT * INTO OUTFILE "/home/hmontes/Documentos/jugadores1.txt"
+    FIELDS TERMINATED BY '\t'
+    LINES TERMINATED BY '\n'
+FROM jugadores1;
+--45
--- BD_Exercises/BD_Second/BD_Imports.sql
+++ BD_Exercises/BD_Second/BD_Imports.sql
@@ -15,6 +15,16 @@ INTO TABLE jugadores2
 LOAD DATA INFILE "/home/hmontes/Documentos/BD_DAM/BD_Exercises/BD_Second/BD_Imports/jugadores3.txt"
 INTO TABLE jugadores3
     COLUMNS TERMINATED BY '\t'
-    OPTIONALLY COLUMNS ENCLOSED BY '\t'
     LINES TERMINATED BY '\n'
     IGNORE 3 LINES
+    (Partidos, Minutos, Puntos, Rebotes, Tapones, Asistencias, Valoracion, NOMBRE);
+--43
+LOAD DATA INFILE "/home/hmontes/Documentos/BD_DAM/BD_Exercises/BD_Second/BD_Imports/jugadores4.txt"
+INTO TABLE jugadores4
+    COLUMNS TERMINATED BY '|'
+    LINES
+    STARTING BY "JUG\t"
+    TERMINATED BY "\n"
+    IGNORE 1 LINES
+
+
