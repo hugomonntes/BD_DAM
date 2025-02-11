2442e4be7fb2e7342cd6ec09084acdf93755c03d
hugomonntes <huugomontees@gmail.com>
2025-02-11 10:49:51 +0100

    Update

--- BD_Exercises/BD_Second/BD_Exports.sql
+++ BD_Exercises/BD_Second/BD_Exports.sql
@@ -4,3 +4,7 @@ SELECT * INTO OUTFILE "/home/hmontes/Documentos/jugadores1.txt"
     LINES TERMINATED BY '\n'
 FROM jugadores1;
 --45
+SELECT * INTO OUTFILE "Documentos/jugadores1.txt"
+    FIELDS TERMINATED BY '|'
+    LINES TERMINATED BY '\n'
+FROM jugadores2;
