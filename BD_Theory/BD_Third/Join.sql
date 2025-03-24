73aaaa544117e72502dbddbcc5362421f9355961
hugomonntes <huugomontees@gmail.com>
2025-03-24 12:59:48 +0100

    Update

--- BD_Theory/BD_Third/Join.sql
+++ BD_Theory/BD_Third/Join.sql
@@ -2,3 +2,5 @@ SELECT * FROM ALUMNOS, TELF; --PRODUCTO CARTESIANO (PREGUNTA DE EXAMEN)
 SELECT empleados.apellido, depart.nombre FROM empleados, depart WHERE empleados.iddepart = depart.iddepart;
 SELECT empleados.apellido, depart.nombre FROM empleados, depart WHERE empleados.iddepart = depart.iddepart AND depart.nombre != "Ventas";
 SELECT * FROM empleados, depart where empleados.iddepart = depart.IDDEPART and depart.LOC LIKE 'Madrid';
+
+-- JOIN (COMBINACIÓN DE TABLAS)
