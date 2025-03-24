6a576811a0b947e4a8863e69fedd067d7b848579
hugomonntes <huugomontees@gmail.com>
2025-03-24 13:05:47 +0100

    Update

--- BD_Theory/BD_Third/Join.sql
+++ BD_Theory/BD_Third/Join.sql
@@ -4,3 +4,5 @@ SELECT empleados.apellido, depart.nombre FROM empleados, depart WHERE empleados.
 SELECT * FROM empleados, depart where empleados.iddepart = depart.IDDEPART and depart.LOC LIKE 'Madrid';
 
 -- JOIN (COMBINACIÓN DE TABLAS)
+SELECT * FROM tabla1 JOIN tabla2 ON tabla1.id = tabla2.id WHERE nombre != "Ventas";
+SELECT * FROM tabla1 JOIN tabla2 USING(id);
