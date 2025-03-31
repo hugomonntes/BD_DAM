8b78b89de2639ef2a07568a209ce21f481a5a954
hugomonntes <huugomontees@gmail.com>
2025-03-31 13:01:33 +0200

    Update

---
+++ BD_Exercises/BD_Third/Tema9/Users.sql
@@ -0,0 +1,10 @@
+-- LA CUENTA DE USER & PASSWORD
+    * Cuenta de User: nombreUser@UbicacionConexion  -- Compuesta por dos parámetros (nombreUser@localhost) (nombreUser@192.0.0.1)
+    * Privilegios: Acciones que puede realizar cada usuario sobre la base de datos
+    * CREATE USER -- Crear Usuario 
+    * DROP USER -- Borrar Usuario
+    * RENAME USER -- Renombrar Usuario
+    * ALTER USER -- Modificar Usuario
+    * SET PASSWORD -- Aplicar Contraseña
+    * GRANT -- Añadir privilegios
+    * REVOKE -- Quitar privilegios
