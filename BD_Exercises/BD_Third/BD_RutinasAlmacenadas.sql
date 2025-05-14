a1c6012c0168d60dc77e9b1cdb3e84eae9de9fa6
hugomonntes <huugomontees@gmail.com>
2025-05-14 11:19:37 +0200

    Update

--- BD_Exercises/BD_Third/BD_RutinasAlmacenadas.sql
+++ BD_Exercises/BD_Third/BD_RutinasAlmacenadas.sql
@@ -1,11 +1,45 @@
--- 1. Crea una función que calcule el cuadrado de un número.
+## 1. Crea una función que calcule el cuadrado de un número.
+
 DELIMITER $$
 CREATE or REPLACE FUNCTION calcularCuadrado(num int) RETURNS double
 BEGIN
 RETURN num * num;
 END $$
 DELIMITER ;
--- 2. Crea una función que pasándole una cadena de texto nos devuelva la cadena
--- concatenada con un guion y su longitud (por ejemplo, con la cadena 'hola'
--- devolverá 'hola - 4'). Tendrá que tener el comentario: "Mi primer procedimiento
--- almacenado".
+
+# Crea una función que pasándole una cadena de texto nos devuelva la cadena
+# concatenada con un guion y su longitud (por ejemplo, con la cadena 'hola'
+# devolverá 'hola - 4'). Tendrá que tener el comentario: "Mi primer procedimiento
+# almacenado".
+DELIMITER $$
+CREATE FUNCTION formatearCadena(cadena varchar(255)) RETURNS varchar(255)
+COMMENT "Mi primer procedimiento almacenado"
+BEGIN
+RETURN CONCAT(cadena,' - ',LENGTH(cadena));
+END $$
+DELIMITER ;
+
+# 4. Crea el procedimiento, sin parámetros, prueba que nos permita listar todos los
+# empleados de la tabla emple.
+DELIMITER $$
+CREATE PROCEDURE mostrarEmpleados()
+BEGIN
+SELECT * FROM tema5.empleados;
+END $$
+DELIMITER ;
+
+# 5. Muestra todos los procedimientos creados.
+SHOW PROCEDURE STATUS;
+
+# 9. Muestra el código de creación del procedimiento prueba.
+SHOW CREATE PROCEDURE mostrarEmpleados;
+
+# 11. Modifica el procedimiento prueba para que tenga el comentario "Procedimiento
+# de prueba" y que se ejecute con los permisos del usuario que lo crea.
+ALTER PROCEDURE mostrarEmpleados SQL SECURITY DEFINER COMMENT "Procedimiento de prueba";
+
+# 14. Crear un procedimiento que liste un número (pasado como parámetro ) de filas
+# de la tabla emple. El procedimiento se ha de ejecutar con los permisos del
+# usuario que lo invoca.
+DELIMITER $$
+CREATE PROCEDURE 
